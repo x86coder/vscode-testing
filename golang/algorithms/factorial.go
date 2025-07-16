23e21ad7779d90529910ce2c359cb783b087f54c
Amed Reyes <reyesrodriguezjaime@gmail.com>
2025-07-15 21:04:05 -0600

    additional test cases + refactor

--- golang/algorithms/factorial.go
+++ golang/algorithms/factorial.go
@@ -1,23 +1,25 @@
 package main
 
-import("fmt")
-import("os")
-import("strconv")
-
+import(
+	"fmt"
+	"os"
+	"strconv"
+)
 func factorial(n uint64) uint64{
 	if n==0 {
-		return 1
+		return 1	// Return standard definition 0! = 1
 	} else{
 		return n * factorial(n-1)
 	}
 }
 
 func main(){
+	fmt.Printf("factorial(n) - Usage: 'go run factorial.go 5'\n")
 	param, err := strconv.ParseUint(os.Args[1], 10, 64)
 	if err == nil {
 		fmt.Printf(" > Calculating factorial of %s\n", os.Args[1])
 		fmt.Printf(" > result = %d\n", factorial(param))
 	} else{
-		fmt.Println("Error converting first parameter")
+		fmt.Println(" ** Error converting first parameter")
 	}
 }
--- golang/algorithms/factorial_test.go
+++ golang/algorithms/factorial_test.go
@@ -3,7 +3,22 @@ package main
 import("testing")
 
 func TestFactorial(t *testing.T){
+	if factorial(0) != 1{
+		t.Errorf("Expected returned value to be: 1")
+	}
+	if factorial(1) != 1{
+		t.Errorf("Expected returned value to be: 1")
+	}
+	if factorial(2) != 2{
+		t.Errorf("Expected returned value to be: 2")
+	}
+	if factorial(3) != 6{
+		t.Errorf("Expected returned value to be: 6")
+	}
+	if factorial(4) != 24{
+		t.Errorf("Expected returned value to be: 1")
+	}
 	if factorial(5) != 120{
-		t.Errorf("Expected 120")
+		t.Errorf("Expected returned value to be: 120")
 	}
 }
